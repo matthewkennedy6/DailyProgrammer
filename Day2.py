148ab27986b0dae74d8099c0bf8819ff019dc636
Matt <matthew.kennedy.6@gmail.com>
2018-02-19 16:23:53 -0500

    Finished Day 2 Easy

--- Day2.py
+++ Day2.py
@@ -1,1 +1,13 @@
+# Dat 2 Intermediate
+
+
+
 # Day 2 Easy
+
+def calculator(mass, acceleration):
+    return mass * acceleration
+
+
+mass = float(input("What is your mass?"))
+acceleration = float(input("What is your acceleration?"))
+print(calculator(mass, acceleration))
